5fd0c190d36548433ade81ed4057cc738131b1ef
jaannis <janis.zalitis.ba@gmail.com>
2018-02-03 01:22:25 +0200

    registracija funkcionē

--- main.js
+++ main.js
@@ -1,22 +1,50 @@
-$('#registration-form').submit(function (event) {
-    var hasErrors = false;
-    $('.errorText').remove();
 
-    var $userName = $('#registration-form input[id="username" ]');
-    if ($userName.val() == '') {
-        hasError = true;
-        var error = '<div class="errorText">Field <b> Username</b> is empty</div>';
-        $userName.parent().append(error);
+function checkPass()
+{
+    
+    var pass1 = document.getElementById('password');
+    var pass2 = document.getElementById('confirmPassword');
+    //Store the Confimation Message Object ...
+    var message = document.getElementById('confirmMessage');
+    //Set the colors we will be using ...
+    var goodColor = "#66cc66";
+    var badColor = "#ff6666";
+    //Compare the values in the password field 
+    //and the confirmation field
+    if(pass1.value == pass2.value){
+        //The passwords match. 
+        //Set the color to the good color and inform
+        //the user that they have entered the correct password 
+        pass2.style.backgroundColor = goodColor;
+        message.style.color = goodColor;
+        message.innerHTML = "Passwords Match"
+    }else{
+        //The passwords do not match.
+        //Set the color to the bad color and
+        //notify the user.
+        pass2.style.backgroundColor = badColor;
+        message.style.color = badColor;
+        message.innerHTML = "Passwords Do Not Match!"
     }
-    var emailElement = 'input[id="email"]';
-    var $email = $(this).find(emailElement);
-    if ($email.val() == '') {
-        hasError = true;
-        var error2 = '<div class="errorText">Field <b> Email</b> is empty</div>';
-        $email.parent().append(error2);
-    }
-    if (hasError == true) {
-        event.preventDefault();
+} 
 
+// validates text only
+function Validate(txt) {
+    txt.value = txt.value.replace(/[^a-zA-Z-'\n\r.]+/g, '');
+}
+// validate email
+function email_validate(email)
+{
+var regMail = /^([_a-zA-Z0-9-]+)(\.[_a-zA-Z0-9-]+)*@([a-zA-Z0-9-]+\.)+([a-zA-Z]{2,3})$/;
+
+    if(regMail.test(email) == false)
+    {
+    document.getElementById("status").innerHTML    = "<span class='warning'>Email address is not valid yet.</span>";
     }
-});
+    else
+    {
+    document.getElementById("status").innerHTML	= "<span class='valid'>Thanks, you have entered a valid Email address!</span>";	
+    }
+}
+
+
